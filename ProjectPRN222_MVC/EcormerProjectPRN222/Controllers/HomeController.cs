7e87cba0e24ece71f9610d32a8276eec2c1a9c84
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-02-26 09:43:56 +0700

    done homeController

--- ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/HomeController.cs
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/HomeController.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcormerProjectPRN222.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcormerProjectPRN222.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyProjectClothingContext _context;
+        public HomeController(MyProjectClothingContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            //New products
+            List<Product> top4ProductsMen = _context.Products.Where(p => p.CategoryId == 6).OrderByDescending(p => p.ProductId).Take(4).ToList();
+            List<Product> top4ProductsWoman = _context.Products.Where(p => p.CategoryId == 7).OrderByDescending(p => p.ProductId).Take(4).ToList();
+            List<Product> top3ProductsChildren = _context.Products.Where(p => p.CategoryId == 8).OrderByDescending(p => p.ProductId).Take(4).ToList();
+            List<Product> top10ProductsNew = top4ProductsMen.Concat(top4ProductsWoman).Concat(top3ProductsChildren).ToList();
+            //best saler
+            List<Product> top5ProductsBestSaler = _context.Products.OrderBy(p => p.Price).Take(10).ToList();
+            ViewBag.Top10ProductsNew = top10ProductsNew;
+            ViewBag.Top10ProductsBestSaler = top5ProductsBestSaler;
             return View();
         }
     }
