72b488acad116bf81369dca9dcd1ae4218d9bb8e
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-03-03 11:47:49 +0700

    Code Shop

--- ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ShopController.cs
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ShopController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcormerProjectPRN222.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcormerProjectPRN222.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly MyProjectClothingContext _context;
+
+        public ShopController(MyProjectClothingContext context)
         {
+            _context = context;
+        }
+        public IActionResult Index(int? idCate)
+        {
+            var categories = _context.Categories.ToList();
+            if (idCate == null)
+            {
+
+                var indexCate = 1;
+                var products = _context.Products.Where(p => p.CategoryId == indexCate).ToList();
+                ViewBag.Products = products;
+                ViewBag.IdCate = indexCate;
+            }
+            else
+            {
+                var indexCate = idCate;
+                var products = _context.Products.Where(p => p.CategoryId == indexCate).ToList();
+                ViewBag.Products = products;
+                ViewBag.IdCate = indexCate;
+            }
+
+            ViewBag.Categories = categories;
             return View();
         }
     }
