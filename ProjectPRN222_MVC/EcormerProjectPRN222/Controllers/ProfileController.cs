885203caeb567d6c6d12223124930bf164e143fb
LeDat29 <aceledat2909@gmail.com>
2025-03-05 08:46:37 +0700

    change password

--- ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ProfileController.cs
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ProfileController.cs
@@ -24,6 +24,7 @@ namespace EcormerProjectPRN222.Controllers
             }
 
             var user = JsonSerializer.Deserialize<Account>(userJson);
+            ViewBag.User = user;
             var account = _context.Accounts.Find(user.UserId); // Lấy từ database
             if (account == null)
             {
@@ -44,6 +45,7 @@ namespace EcormerProjectPRN222.Controllers
             }
 
             var user = JsonSerializer.Deserialize<Account>(userJson);
+            ViewBag.User = user;
             var account = _context.Accounts.Find(user.UserId);
             if (account == null)
             {
@@ -81,6 +83,8 @@ namespace EcormerProjectPRN222.Controllers
                 // Cập nhật session
                 var userJson = JsonSerializer.Serialize(existingAccount);
                 HttpContext.Session.SetString("user", userJson);
+                var user = JsonSerializer.Deserialize<Account>(userJson);
+                ViewBag.User = user;
 
                 ViewBag.UpdateStatus = "Success";
             }
@@ -107,6 +111,8 @@ namespace EcormerProjectPRN222.Controllers
             }
 
             var user = JsonSerializer.Deserialize<Account>(userJson);
+            ViewBag.User = user;
+
             var account = _context.Accounts.Find(user.UserId);
             if (account == null)
             {
@@ -126,6 +132,8 @@ namespace EcormerProjectPRN222.Controllers
             if (userJson != null)
             {
                 var user = JsonSerializer.Deserialize<Account>(userJson);
+                ViewBag.User = user;
+
                 var account = _context.Accounts.Find(user.UserId);
 
                 if (account != null)
@@ -137,6 +145,103 @@ namespace EcormerProjectPRN222.Controllers
             }
 
             return RedirectToAction("Index", "Home");
+        }
+        //GET: change pass
+        public IActionResult ChangePassword()
+        {
+            var userJson = HttpContext.Session.GetString("user");
+            if (userJson == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var user = JsonSerializer.Deserialize<Account>(userJson);
+            ViewBag.User = user;
+
+            var account = _context.Accounts.Find(user.UserId);
+            if (account == null)
+            {
+                HttpContext.Session.Remove("user");
+                return RedirectToAction("Index", "Login");
+            }
+            var userJsonUpdated = JsonSerializer.Serialize(account);
+            HttpContext.Session.SetString("user", userJsonUpdated);
+
+            return View(account);
+        }
+
+        // POST: Profile/Edit
+        [HttpPost]
+        public IActionResult ChangePassword(string currentPassword, string newPassword, string confirmPassword)
+        {
+
+            try
+            {
+                if (currentPassword.Equals(newPassword) )
+                {
+                    //ghi user vao session
+                    var userJson = HttpContext.Session.GetString("user");
+                    var user = JsonSerializer.Deserialize<Account>(userJson);
+                    ViewBag.User = user;
+
+                    var account = _context.Accounts.Find(user.UserId);
+                    ViewBag.UpdateStatus = "The new password same as old password";
+                    var userJsonUpdated = JsonSerializer.Serialize(account);
+                    HttpContext.Session.SetString("user", userJsonUpdated);
+
+                }
+                if(newPassword.Equals(confirmPassword)) 
+                {
+                    var userJson = HttpContext.Session.GetString("user");
+                    var user = JsonSerializer.Deserialize<Account>(userJson);
+                    ViewBag.User = user;
+
+                    var account = _context.Accounts.Find(user.UserId);
+                    ViewBag.UpdateStatus = "The confirm password isn't same as new password";
+                    var userJsonUpdated = JsonSerializer.Serialize(account);
+                    HttpContext.Session.SetString("user", userJsonUpdated);
+                }
+                else
+                {
+                    var userJson = HttpContext.Session.GetString("user");
+                    var user = JsonSerializer.Deserialize<Account>(userJson);
+
+
+                    var existingAccount = _context.Accounts.FirstOrDefault(a => a.UserId == user.UserId);
+                    if (existingAccount == null)
+                    {
+                        ViewBag.UpdateStatus = "Fail: Account not found";
+                        return View(user);
+                    }
+
+                    // Cập nhật giá trị mới vào tài khoản đang tracking
+
+                    existingAccount.Password = newPassword;
+
+                    _context.Accounts.Update(existingAccount);
+
+                    _context.SaveChanges();
+
+                    // Cập nhật session
+                    userJson = JsonSerializer.Serialize(existingAccount);
+                    HttpContext.Session.SetString("user", userJson);
+                    user = JsonSerializer.Deserialize<Account>(userJson);
+
+                    ViewBag.User = user;
+
+                }
+
+            }
+            catch (DbUpdateException ex)
+            {
+                ViewBag.UpdateStatus = "Fail: " + ex.InnerException?.Message;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.UpdateStatus = "Fail: " + ex.Message;
+            }
+
+            return View();
         }
     }
 }
