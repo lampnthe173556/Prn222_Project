ff058b8e2235bdc19838e5991eb56b65827f2862
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-03-06 22:53:51 +0700

    update productDetails

---
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Models/ProductDetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcormerProjectPRN222.Models
+{
+    public class ProductDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
