5adfc23ca5a885ae23bc1b26f26c53ebe3f82da9
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2025-03-07 17:10:41 +0700

    done register

---
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using EcormerProjectPRN222.Dao.AccountDAO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcormerProjectPRN222.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult RegisterIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegisterIndex(String userName, String password, String fullName, String email, String phone, String location)
+        {
+            if(AccountDAO.checkDuplicate(email))
+            {
+                ViewBag.MessageError = "Email already exists!";
+                return View("RegisterIndex");
+            }else if(AccountDAO.checkDuplicateUserName(userName))
+            {
+                ViewBag.MessageError = "Username already exists!";
+                return View("RegisterIndex");
+            }
+            else
+            {
+                AccountDAO.RegisterAccount(userName, password, fullName, email, phone, location);
+            }
+            return RedirectToAction("Index", "Login");  
+        }
+    }
+}
