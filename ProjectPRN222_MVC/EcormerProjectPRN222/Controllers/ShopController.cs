2d749c05f553ca3a640561f7cfa65062ff34b10b
LeDat29 <aceledat2909@gmail.com>
2025-03-16 00:24:14 +0700

    update shop

--- ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ShopController.cs
+++ ProjectPRN222_MVC/EcormerProjectPRN222/Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 ﻿using EcormerProjectPRN222.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace EcormerProjectPRN222.Controllers
 {
@@ -29,9 +30,36 @@ namespace EcormerProjectPRN222.Controllers
                 ViewBag.Products = products;
                 ViewBag.IdCate = indexCate;
             }
+            //account
+            var userJson = HttpContext.Session.GetString("user");
+            if (userJson == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var user = JsonSerializer.Deserialize<Account>(userJson);
+            ViewBag.User = user;
+            // update số lượng sản phẩm trong giỏ hàng
+            int? userId = GetUserSession();
+            if (userId == null) return RedirectToAction("Index", "Login"); // ko có ID thì về trang Login
+
+            var cartItems = _context.CartItems.Where(c => c.UserId == Convert.ToInt32(userId)).ToList();
+            ViewBag.cartItems = cartItems;
 
             ViewBag.Categories = categories;
             return View();
+
+        }
+
+        public int GetUserSession()
+        {
+            var userJson = HttpContext.Session.GetString("user");
+            if (userJson != null)
+            {
+                var user = JsonSerializer.Deserialize<Account>(userJson);
+                return user.UserId; // Trả về UserId
+            }
+            return 0;
         }
     }
 }
